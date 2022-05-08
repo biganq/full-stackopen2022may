00d094e5d8acc3c57163908f2e31cdfeb80ab9f7
xbox366 <54818355@qq.com>
2022-05-08 23:51:25 +0800

    2nd

--- part1/src/App.js
+++ part1/src/App.js
@@ -31,24 +31,83 @@
 //   )
 // }
 
-const Hello = (props)=>{
-  return(
-    <div>
-      <p>Hello {props.name},you are {props.age} years old</p>
-    </div>
-  )
-}
+// const Hello = (props)=>{
+//   return(
+//     <div>
+//       <p>Hello {props.name},you are {props.age} years old</p>
+//     </div>
+//   )
+// }
+// const App = () =>{
+//   const name = "niuli"
+//   const age = 18
+//   return(
+//     <div>
+//       <h1>Greeting</h1>
+//       <Hello name={name} age={age}/>
+//       <Hello name="anqi" age={23} />
+//     </div>
+//   )
+// }
+
+// const Hello = (props) => {
+//   const {name,age} = props
+//   const bornYear = () =>{
+//     const year = new Date().getFullYear()
+//     return year-age
+//   }
+//   return (
+//     <div>
+//       <p>
+//         Hello {name}, you are {age} years old.
+//         So you were born in {bornYear()}
+//       </p>
+//     </div>
+//   )
+// }
+
+// const App = () => {
+//   const name = 'Peter'
+//   const age = 10
+
+//   return (
+//     <div>
+//       <h1>Greetings</h1>
+//       <Hello name="Maya" age={26 + 10} />
+//       <Hello name={name} age={age} />
+//       <Hello name="liudehua" age={55} />
+//     </div>
+//   )
+// }
+import { useState } from "react";
+// const App = (props) =>{
+//   const {counter} = props
+//   const [counters,setCounters] = useState(counter)
+//   setTimeout(() => {
+//     setCounters(counters+1)
+//   }, 1000);
+//   console.log("rerendering..."+counters)
+//   return(
+//     <div>
+//       <h1>{counters}</h1>
+//     </div>
+//   )
+// }
 const App = () =>{
-  const name = "niuli"
-  const age = 18
+  const [counter,setCounter] = useState(0)
+  const increaseByOne = () =>{
+    console.log("clicked")
+    setCounter(counter+1)
+  }
+  const setToZero = () =>{
+    setCounter(0)
+  }
   return(
     <div>
-      <h1>Greeting</h1>
-      <Hello name={name} age={age}/>
-      <Hello name="anqi" age={23} />
+      <p>{counter}</p>
+      <button onClick={increaseByOne}>plus</button>
+      <button onClick={setToZero}>zero</button>
     </div>
   )
 }
-
-
 export default App;
--- part1/src/index.js
+++ part1/src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let counter = 444
+
+
+ReactDOM.render( < App counter={counter} / > ,document.getElementById('root'))
+
